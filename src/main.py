314f5294b1c6e68f0b5c7171aadce0cd0161065c
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2025-04-09 14:20:12 +0200

    Add class diagram.

---
+++ src/main.py
@@ -0,0 +1,17 @@
+"""Main..."""
+
+
+class MainGame:
+    """Main game class definition."""
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        """Main game loop."""
+        pass
+
+
+if __name__ == "__main__":
+    game = MainGame()
+    game.run()
